a4bd891b64ac6a9bb36a46b2cca5e8c96ffbad2d
Bartek <bartekk.jonca@gmail.com>
2021-10-30 15:43:02 +0200

    Case sensitivity bug fix

---
+++ src/Styles/Components/Button.module.scss.d.ts
@@ -0,0 +1,4 @@
+export const button: string;
+export const buttonGreen: string;
+export const buttonSideRight: string;
+export const buttonMobileFullSize: string;
---
+++ src/Styles/Components/Loader.module.scss.d.ts
@@ -0,0 +1,5 @@
+export const loader: string;
+export const loaderFullScreen: string;
+export const loaderHidden: string;
+export const loader__animation: string;
+export const loaderAnimation: string;
